f12e4192f8f7ff0d236e9c02a70d0f7853e6eb9f
Сысоева Дарья <77782739+ssvdrv@users.noreply.github.com>
2021-10-25 19:43:25 +0700

    Add files via upload

---
+++ Pr-25-master/app/src/main/java/com/example/pr_25/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.pr_25;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    public void Print(float num1, float num2, float result){
+        tvResult.setText(num1 + " " + oper + " " + num2 + " = " + result);
+    }
+
+    final int MENU_RESET_ID = 1;
+    final int MENU_QUIT_ID = 2;
+
+    EditText etNum1;
+    EditText etNum2;
+
+    Button btnAdd;
+    Button btnSub;
+    Button btnMult;
+    Button btnDiv;
+
+    TextView tvResult;
+
+    String oper = "";
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        menu.add(0, MENU_QUIT_ID, 0, "Quit");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_RESET_ID:
+                // очищаем поля
+                etNum1.setText("");
+                etNum2.setText("");
+                tvResult.setText("");
+                break;
+            case MENU_QUIT_ID:
+                // выход из приложения
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etNum1 = (EditText) findViewById(R.id.etNum1);
+        etNum2 = (EditText) findViewById(R.id.etNum2);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSub = (Button) findViewById(R.id.btnSub);
+        btnMult = (Button) findViewById(R.id.btnMult);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
+
+        tvResult = (TextView) findViewById(R.id.tvResult);
+
+        btnAdd.setOnClickListener(this);
+        btnSub.setOnClickListener(this);
+        btnMult.setOnClickListener(this);
+        btnDiv.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        float num1 = 0;
+        float num2 = 0;
+        float result = 0;
+
+        if (TextUtils.isEmpty(etNum1.getText().toString())
+                || TextUtils.isEmpty(etNum2.getText().toString())) {
+            return;
+        }
+
+        num1 = Float.parseFloat(etNum1.getText().toString());
+        num2 = Float.parseFloat(etNum2.getText().toString());
+
+        switch (v.getId()) {
+            case R.id.btnAdd:
+                oper = "+";
+                result = num1 + num2;
+                Print(num1, num2, result);
+                break;
+            case R.id.btnSub:
+                oper = "-";
+                result = num1 - num2;
+                Print(num1, num2, result);
+                break;
+            case R.id.btnMult:
+                oper = "*";
+                result = num1 * num2;
+                Print(num1, num2, result);
+                break;
+            case R.id.btnDiv:
+                if(num2 == 0){
+                    tvResult.setText("На ноль делить нельзя!");
+                }else{
+                    oper = "/";
+                    result = num1 / num2;
+                    Print(num1, num2, result);
+                }
+                break;
+            default:
+        }
+    }
+}
